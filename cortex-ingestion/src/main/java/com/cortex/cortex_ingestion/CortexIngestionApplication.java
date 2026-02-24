86cb31af4339ed44aff1a008365b5c49e604fe39
Varad Rampurikar <set>
2026-02-24 21:34:26 +0530

    covering common lib for entity scanning

--- cortex-ingestion/src/main/java/com/cortex/cortex_ingestion/CortexIngestionApplication.java
+++ cortex-ingestion/src/main/java/com/cortex/cortex_ingestion/CortexIngestionApplication.java
@@ -3,7 +3,12 @@ package com.cortex.cortex_ingestion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
+@EntityScan(basePackages = { "com.cortex.cortex_ingestion.model", "com.cortex.cortex_common.model" })
+@EnableJpaRepositories(basePackages = { "com.cortex.cortex_ingestion.repository" })
 public class CortexIngestionApplication {
 
 	public static void main(String[] args) {
--- cortex-media-processing-service/src/main/java/com/cortex/cortex_media_processing_service/CortexMediaProcessingServiceApplication.java
+++ cortex-media-processing-service/src/main/java/com/cortex/cortex_media_processing_service/CortexMediaProcessingServiceApplication.java
@@ -3,7 +3,12 @@ package com.cortex.cortex_media_processing_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
+@EntityScan(basePackages = { "com.cortex.cortex_media_processing_service.model", "com.cortex.cortex_common.model" })
+@EnableJpaRepositories(basePackages = { "com.cortex.cortex_media_processing_service.repository" })
 public class CortexMediaProcessingServiceApplication {
 
 	public static void main(String[] args) {
