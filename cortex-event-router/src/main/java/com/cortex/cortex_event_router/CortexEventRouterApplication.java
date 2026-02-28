650d7af6888db416db016db3e69d9094502cb391
Varad Rampurikar <set>
2026-02-28 14:00:18 +0530

    excluding jdbc props from auto configuration

--- cortex-event-router/src/main/java/com/cortex/cortex_event_router/CortexEventRouterApplication.java
+++ cortex-event-router/src/main/java/com/cortex/cortex_event_router/CortexEventRouterApplication.java
@@ -3,7 +3,14 @@ package com.cortex.cortex_event_router;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+		"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+		"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+		"org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration",
+		"org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration",
+		"org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration",
+		"org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration"
+})
 public class CortexEventRouterApplication {
 
 	public static void main(String[] args) {
