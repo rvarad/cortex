e2606b012cfc2a13bd1dc4e18b2dfb42ce4c1ca0
Varad Rampurikar <set>
2026-06-09 16:23:25 +0530

    fix: adding correct import for mdc

--- cortex-media-processing-service/src/main/java/com/cortex/cortex_media_processing_service/consumer/MediaEventConsumer.java
+++ cortex-media-processing-service/src/main/java/com/cortex/cortex_media_processing_service/consumer/MediaEventConsumer.java
@@ -1,6 +1,6 @@
 package com.cortex.cortex_media_processing_service.consumer;
 
-import org.jboss.logging.MDC;
+import org.slf4j.MDC;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
@@ -22,7 +22,7 @@ public class MediaEventConsumer {
 
   @KafkaListener(topics = "${app.kafka.topic.media}", groupId = "cortex-media-processing-group")
   public void consume(FileIngestionEventDTO event) throws InterruptedException, Exception {
-    MDC.put("fileId", event.getFileId());
+    MDC.put("fileId", event.getFileId().toString());
     try {
       log.info("Received file ingestion event for fileId: {}", event.getFileId());
       mediaProcessingExecutor.execute(() -> mediaProcessingService.processMedia(
